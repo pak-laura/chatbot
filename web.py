52bea3dd4a59d7774b8dadb6fd210a7d471f32b2
haquemaliha <haquemaliha@yahoo.com>
2018-09-24 13:26:00 -0500

    Got 15 relevant urls

--- web.py
+++ web.py
@@ -1,24 +1,40 @@
-# ??
+
 
 from bs4 import BeautifulSoup
 import requests
 
-# starter_url = "https://travel.usnews.com/rankings/worlds-best-vacations/"
-starter_url = "https://www.britannica.com/place/Turkey"
-# starter_url = "https://travel.state.gov/content/travel/en/international-travel/International-Travel-Country-Information-Pages/Turkey.html"
-r = requests.get(starter_url)
 
-data = r.text
-soup = BeautifulSoup(data, "lxml")
+def mainUrl():
+    starter_url = "https://www.britannica.com/place/Turkey"
+    r = requests.get(starter_url)
+    data = r.text
+    soup = BeautifulSoup(data, "lxml")
+
+    mainUrls = set()
+    urlCount = 0
+    for link in soup.find_all('a'):
+        link_str = str(link.get('href'))
+        if 'Turkey' in link_str or 'turkey' in link_str:
+            if 'turkey-bird' not in link_str:
+                #if the string is from same domain and does not include #--will not get copies of same page
+                if link_str.startswith("/") and "#" not in link_str and len(mainUrls) < 15:
+                    mainUrls.add(starter_url + str(link.get('href')))
+                if link_str.startswith("https:") and urlCount < 7 and len(mainUrls) < 15:
+                    urlCount += 1
+                    mainUrls.add(str(link.get('href')))
+                
+    print (mainUrls)
+    print(len(mainUrls))
+    print("end of crawler")
+    return(mainUrls)
 
 
-# write urls to a file
-with open('urls.txt', 'w') as f:
-   for link in soup.find_all('a'):
-      link_str = str(link.get('href'))
-      if 'Turkey' in link_str:
-         print(link.get('href'))
-         f.write(str(link.get('href')) + '\n\n')
 
-# end of program
-print("end of crawler")
+def main():
+
+    #return list of 15 relevant urls
+    mainUrls = mainUrl()
+
+
+if __name__ == '__main__':
+    main()
